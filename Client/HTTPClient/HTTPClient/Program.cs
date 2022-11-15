193a490a6e66aff6a51d3f4d5e18547fc1a92355
DianaO20 <74967560+DianaO20@users.noreply.github.com>
2022-11-15 20:05:24 +0200

    Add files via upload

---
+++ Client/HTTPClient/HTTPClient/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace HttpClientApp
+{
+    class HttpClientApp
+    {
+        private static Uri uri = new Uri("http://localhost:8000/");
+        static HttpClient client = new HttpClient() { BaseAddress = uri };
+        static string fileName = @"C:\Users\Diana\Documents\output.txt";
+        static async Task Main(string[] args)
+        {
+            //Here we check if an output file exists on that location, and if yes, we delete it, because we will create a new one
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+            }
+
+            HttpClientApp program = new HttpClientApp();
+            //Send request to the server
+            await program.SendRequest();
+            //Our work is done, send request to the server to shutdown
+            await client.PostAsync(String.Concat(uri.ToString(), "shutdown"), null);
+        }
+
+        private async Task SendRequest()
+        {
+            string response = await client.GetStringAsync(uri);
+            //We call this method to store the data that we recieved in the file.
+            ProcessRequest(response);
+            //Here we send a response with the number of tasks we have in the file to the server
+            await SendResponse();
+        }
+
+        private void ProcessRequest(string response)
+        {
+            Console.WriteLine(response);
+            WriteToFile(response);
+        }
+
+        private void WriteToFile(string text)
+        {
+            using (FileStream fs = File.Create(fileName))
+            {
+                // Add some text to file    
+                byte[] input = new UTF8Encoding(true).GetBytes(text);
+                fs.Write(input, 0, input.Length);
+            }
+        }
+
+        private async Task SendResponse()
+        {
+            string result = "";
+            using (StreamReader sr = File.OpenText(fileName))    
+        {    
+            string s = "";
+            while ((s=sr.ReadLine()) != null)    
+            {
+                    result = String.Concat(result, s);    
+            }    
+        } 
+            await client.PostAsync(String.Concat(uri.ToString(), "extract"), 
+                new StringContent(String.Concat("Number of tasks currently in file: ", ToNumberOfTasks(result).ToString())));
+        }
+
+        private int ToNumberOfTasks(string output)
+        {
+            String[] tasks = output.Split(" ", 10);
+            return tasks.Count();
+        }
+
+    }
+}
---
+++ Server/HTTPServer/HTTPServer/Program.cs
@@ -0,0 +1,133 @@
+﻿
+using System;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace HttpListenerExample
+{
+    class HttpServer
+    {
+        static HttpListenerResponse resp;
+        static byte[] data;
+        static string output = "";
+        static int i;
+        public static HttpListener listener;
+        public static string url = "http://localhost:8000/";
+        public static int pageViews = 0;
+        public static int requestCount = 0;
+
+        public static async Task HandleIncomingConnections()
+        {
+            bool runServer = true;
+
+            // While a user hasn't visited the `shutdown` url, keep on handling requests
+            while (runServer)
+            {
+                // Will wait here until we hear from a connection
+                HttpListenerContext ctx = await listener.GetContextAsync();
+
+                // Peel out the requests and response objects
+                HttpListenerRequest req = ctx.Request;
+                resp = ctx.Response;
+
+                // Print out some info about the request
+                Console.WriteLine("Request #: {0}", ++requestCount);
+                Console.WriteLine(req.Url.ToString());
+                Console.WriteLine(req.HttpMethod);
+                Console.WriteLine(req.UserHostName);
+                Console.WriteLine(req.UserAgent);
+                Console.WriteLine();
+
+                if ((req.HttpMethod == "POST") && (req.Url.AbsolutePath == "/shutdown"))
+                {
+                    Console.WriteLine("Shutdown requested");
+                    runServer = false;
+                }
+
+                else if ((req.HttpMethod == "POST") && (req.Url.AbsolutePath == "/extract"))
+                {
+                    Console.WriteLine("Recieved extracted data:");
+                    ShowRequestData(req);
+                }
+                //Here we put the threads to work and generate some data
+                StartMultiThreading();
+
+                //Here we build the response and send it to the client
+                Thread.Sleep(5000);
+                data = Encoding.UTF8.GetBytes(output);
+                resp.ContentType = "text/html";
+                resp.ContentEncoding = Encoding.UTF8;
+                resp.ContentLength64 = data.LongLength;
+                await resp.OutputStream.WriteAsync(data, 0, data.Length);
+                resp.Close();
+            }
+        }
+
+        public static void StartMultiThreading()
+        {
+            for (i = 0; i <= 10; i++)
+            {
+                Thread thread = new Thread(() => WriteThread(i));
+                thread.Start();
+            }
+        }
+        public static void WriteThread(int i)
+        {
+                if (output == "")
+                {
+                    output = String.Concat("Task", i.ToString());
+                }
+                else if (output != null)
+                {
+                    output = String.Concat(output," ", "Task", i.ToString());
+                }
+        }
+
+        public static void ShowRequestData(HttpListenerRequest request)
+        {
+            if (!request.HasEntityBody)
+            {
+                Console.WriteLine("No client data was sent with the request.");
+                return;
+            }
+            System.IO.Stream body = request.InputStream;
+            System.Text.Encoding encoding = request.ContentEncoding;
+            System.IO.StreamReader reader = new System.IO.StreamReader(body, encoding);
+            if (request.ContentType != null)
+            {
+                Console.WriteLine("Client data content type {0}", request.ContentType);
+            }
+            Console.WriteLine("Client data content length {0}", request.ContentLength64);
+
+            Console.WriteLine("Start of client data:");
+            // Will convert the data to a string and display it on the console.
+            string s = reader.ReadToEnd();
+            Console.WriteLine(s);
+            Console.WriteLine("End of client data:");
+            body.Close();
+            reader.Close();
+            // If it is finished with the request, it should be closed also.
+        }
+
+
+
+
+        public static void Main(string[] args)
+        {
+            // Will create a Http server and start listening for incoming connections
+            listener = new HttpListener();
+            listener.Prefixes.Add(url);
+            listener.Start();
+            Console.WriteLine("Listening for connections on {0}", url);
+
+            // Will handle requests
+            Task listenTask = HandleIncomingConnections();
+            listenTask.GetAwaiter().GetResult();
+
+            // Will close the listener
+            listener.Close();
+        }
+    }
+}
